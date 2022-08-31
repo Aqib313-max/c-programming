bb9c80137f6ac831ede0a50f81b506679000b028
Aqib Nazir <108110264+Aqib313-max@users.noreply.github.com>
2022-08-31 10:09:41 +0530

    Add files via upload

---
+++ difference  two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c; 
+	printf("enter any two numbers");                         // enter the users need initially.
+	scanf("%i%i",&a,&b);
+//	a=221;
+//	b=121;
+	c=a-b;
+	printf("the difference of two numbers is %i",c);
+	return 0;
+}
